8e524e721533fba2fe6574e662d9e9be126e8298
cuihdb <cuihdb@yonyou.com>
2016-02-02 13:01:16 +0800

    testLib
    测试打包静态库

---
+++ Example/Pods/Target Support Files/HYBLib/HYBLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HYBLib : NSObject
+@end
+@implementation PodsDummy_HYBLib
+@end
---
+++ Example/Pods/Target Support Files/HYBLib/HYBLib-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "HYBNewsCell.h"
+#import "HYBNewsModel.h"
+
+FOUNDATION_EXPORT double HYBLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char HYBLibVersionString[];
+
